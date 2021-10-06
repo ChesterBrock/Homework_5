cba13cb3c5dbe2fde5f0164ea58d407a6f0593c9
ChesterBrock <cj.rozor@gmail.com>
2021-10-05 21:18:54 -0700

    Add project files.

---
+++ Part1/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿/// Chapter No. 5		Exercise No. 1
+/// File Name: MainWindow.xaml.cs
+/// @author: Chester Brock 
+/// Date:  Sep 20, 2021
+///
+/// Problem Statement: calculates a person’s BMI and prints a message telling whether they below, within, or above the healthy range.
+/// 
+/// 
+/// Overall Plan:
+/// 1) Prompt for the users weight and height 
+/// 2) use the formula weight (lb) / height (in) / height (in)] x 720.
+/// 3) display the results (blue) - under (green) - met (yellow) - over
+/// 
+///
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Part1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int weight = Int32.Parse(YourWeightTextBox.Text);
+            int Height = Int32.Parse(YourHeightTextBox.Text);
+
+            // Calculations for converting to BMI 
+            int bmi = (weight * 703)/(Height * Height) * 7;
+
+            if(bmi < 15.0)
+            {
+                Canvas.Background = Brushes.Blue;
+                MessageBox.Show("Below BMI: " + bmi);
+            }
+            else if (bmi >=15.0 && bmi<= 26.0)
+            {
+                Canvas.Background = Brushes.Green;
+                MessageBox.Show("Good BMI: " + bmi);                
+            }
+            else if(bmi < 26.0)
+            {
+                Canvas.Background = Brushes.Yellow;
+                MessageBox.Show("BMI Over: " + bmi);                
+            }
+            else
+            {
+                Canvas.Background = Brushes.Red;
+                MessageBox.Show("Invalid Input ");                
+            }
+        }
+    }
+}
---
+++ Part2/Program.cs
@@ -0,0 +1,55 @@
+﻿/// Chapter No. 5		Exercise No. 2
+/// File Name: Program.cs
+/// @author: Chester Brock 
+/// Date:  Sep 20, 2021
+///
+/// Problem Statement:  print out statistics for eight coin tosses.
+/// 
+/// 
+/// Overall Plan:
+/// 1) Prompt the user for either heads or tails 
+/// 2) Loop though 8 times and see if the users places h or t
+/// 3) Calculate and print out the total percentage for the flips 
+///
+/// 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int heads = 0, tails = 0;
+            Console.WriteLine("This is a Heads and Tails simulator. Please enter h or t 8 times");
+            // Create a loop that will prompt the user 8 times to enther either heads or tails
+            for(int i = 1; i <= 8; i++)
+            {
+                Console.WriteLine("#" + i + " Please enter a h or t. ");
+                if(Console.ReadLine() == "h")
+                {
+                    heads++;
+                }
+                else
+                {
+                    tails++;
+                }
+            }
+            Console.WriteLine("Number of heads = " + heads + "\n");
+            Console.WriteLine("Number of tails = " + tails + "\n");
+
+            //Percentages
+            double headPercent = Convert.ToDouble(heads) / 8 * 100 ;
+            double tailPercent = Convert.ToDouble(tails) / 8 * 100;
+            Console.WriteLine("Percent heads: " + headPercent);
+            Console.WriteLine("Percent tails: " + tailPercent);
+
+            Console.ReadLine();
+        }
+    }
+}
